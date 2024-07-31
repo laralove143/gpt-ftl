927f9077a7dabba79dec030843a45b1e638a2337
Lara Kayaalp <me@lara.lv>
2024-07-31 21:50:07 +0300

    implement static text translation

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from os import environ
+
+
+load_dotenv()
+base_lang = environ.get("BASE_LANG")
+model = environ.get("MODEL")
+root = environ.get("FTL_ROOT_PATH")
+
+client = OpenAI()
+
+system_messages = {
+    "role": "You are a translator.",
+    "assignment": "In the content you are given, each value is assigned to a variable with =, do not alter the "
+    "variable names or the = sign.",
+}
+
+
+def files(lang):
+    return [
+        filename
+        for filename in os.listdir(os.path.join(root, lang))
+        if filename.endswith(".ftl")
+    ]
+
+
+if __name__ == "__main__":
+    base_files = files(base_lang)
+    languages = [
+        lang
+        for lang in os.listdir(root)
+        if os.path.isdir(os.path.join(root, lang)) and lang != base_lang
+    ]
+
+    system_messages = [system_messages["role"], system_messages["assignment"]]
+
+    messages = [{"role": "system", "content": content} for content in system_messages]
+
+    user_message_start = f"Translate the following content to {languages}:\n"
+
+    for file in base_files:
+        with open(os.path.join(root, base_lang, file), "r") as f:
+            content = f.read()
+
+            messages.append({"role": "user", "content": user_message_start + content})
+
+            translation = (
+                client.chat.completions.create(model=model, messages=messages)
+                .choices[0]
+                .message.content
+            )
